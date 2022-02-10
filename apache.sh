76f33e137d6b9272846a0dd6df438f0b20208ccb
Olabode, Osinubi <Olabodeosinubi@gmail.com>
2022-02-10 14:17:38 -0500

    Added apache sh file

---
+++ apache.sh
@@ -0,0 +1,10 @@
+#! /bin/bash
+yum update httpd -y
+yum install httpd -y
+systemctl start httpd
+systemctl enable httpd
+echo "Welcome To Oshieset Technology!!" > /var/www/html/index.html
+firewall-cmd --permanent --add-service=http
+firewall-cmd --permanent --add-port=80/tcp
+firewall-cmd --reload
+systemctl restart httpd
