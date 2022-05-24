1885d40eb063ed45cacb68efc123a9d89874df68
root <root@lnx41.king.local>
2022-05-23 20:26:35 -0400

    I edited apache.sh

--- apache.sh
+++ apache.sh
@@ -3,7 +3,7 @@ yum update httpd -y
 yum install httpd -y
 systemctl start httpd
 systemctl enable httpd
-echo "Welcome To Oshieset Technology and Enjoy Our Services, don't forget to come back. Sure I will. Thanks" > /var/www/html/index.html
+echo "Welcome To Oshieset Technology and Enjoy Our Services, don't forget to come back. Sure I will. Thanks. I'm back" > /var/www/html/index.html
 firewall-cmd --permanent --add-service=http
 firewall-cmd --permanent --add-port=80/tcp
 firewall-cmd --reload
